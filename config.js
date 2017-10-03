74d90b46e16471a496d092f8c1e81bd4c7472e24
aaron <pleaseshutupaaron@gmail.com>
2017-10-03 18:38:57 -0400

    added mLab

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/howdyNeighbor';
+                      'mongodb://demo:demo@ds161164.mlab.com:61164/howdyneighbor';
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                      		'mongodb://localhost/howdyNeighborTest';
+                      		'mongodb://demo:demo@ds161584.mlab.com:61584/howdyneighbortest';
 
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
